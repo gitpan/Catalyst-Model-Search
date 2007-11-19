a51059aa5e87383276f3987b2ff8d5413da2dbbe
Marcus Ramberg <mramberg@cpan.org>
2007-11-19 19:54:13 +0000

    Import of MRAMBERG/Catalyst-Model-Search-0.03 from CPAN.
    gitpan-cpan-distribution: Catalyst-Model-Search
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MRAMBERG/Catalyst-Model-Search-0.03.tar.gz
    gitpan-cpan-author:       MRAMBERG
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Model/Search.pm
+++ lib/Catalyst/Model/Search.pm
@@ -4,6 +4,8 @@ use strict;
 use NEXT;
 use base qw/Catalyst::Base/;
 
+our $VERSION='0.03';
+
 sub new { 
     my ( $self, $c ) = @_;
 
--- lib/Catalyst/Model/Search/Plucene.pm
+++ lib/Catalyst/Model/Search/Plucene.pm
@@ -1,6 +1,6 @@
 package Catalyst::Model::Search::Plucene;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use strict;
 use warnings;
